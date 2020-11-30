69c0b6327bc2bfecbd3b3682367a7bd15f29e007
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2020-11-30 08:55:46 +0100

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -4,11 +4,16 @@ Created by NeKro
 
 
 #import
-import pynput, time
+import time
+import threading
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+
 
 #init
 delay = float(input("delay :"))
 printdelay = str(delay)
-
 print("\ndelay = " + printdelay)
 
+click = Button.left
+activate = KeyCode(char='²')
