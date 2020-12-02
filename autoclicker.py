3f0331f328816f82e62a2bcac42463c72afcb671
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2020-12-02 12:29:08 +0100

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -15,12 +15,20 @@ print("\ndelay = " + printdelay)
 button = Button.left
 mouse = Controller()
 click = int(0)
-
+start = int(1)
 
 while True:
     if click == 1:
         mouse.press(button)
         mouse.release(button)
         time.sleep(delay)
+    
+    elif start == 1:
+        if click == 1:
+            pass
+        if click == 0:
+            click =+1
+        
+
     else :
         pass
